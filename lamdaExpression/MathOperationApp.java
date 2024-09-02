8d4c578a69e55eda03faca4d4ed1f68af41df829
tejaswini_14 <shelaketejaswini14@gmail.com>
2024-09-02 13:17:20 +0530

    Added: iterate through list and print only even numbers from it

--- lamdaExpression/MathOperationApp.java
+++ lamdaExpression/MathOperationApp.java
@@ -1,6 +1,6 @@
 package com.lamdaExpression;
 import java.util.*;
-import java.util.function.Consumer;
+import java.util.function.Predicate;
 
 public class MathOperationApp {
 
@@ -8,43 +8,17 @@ public class MathOperationApp {
 
         List<Integer> list = new ArrayList<>();
         list.add(10);
-        list.add(20);
+        list.add(21);
         list.add(30);
-        list.add(40);
+        list.add(41);
         list.add(50);
 
-        // Method 1: Traversing using iterator
-        Iterator<Integer> it = list.iterator(); 
-        while (it.hasNext()) {
-            Integer n = it.next();
-            System.out.println("method1: Iterator value :: " + n);
-        }
-
-        // Method 2: Traversing with Consumer interface implementation
-        class MyConsumer implements Consumer<Integer> {
-            public void accept(Integer t) {
-                System.out.println("method2: Consumer implementation value :: " + t);
-            }
-        }
-        MyConsumer action = new MyConsumer();
-        list.forEach(action);
-
-        // Method 3: Traversing with anonymous Consumer interface implementation
-        list.forEach(new Consumer<Integer>() {
-            public void accept(Integer t) {
-                System.out.println("method3: For each anonymous class value :: " + t);
-            }
-        });
-
-        // Method 4: Explicit lambda function
-        Consumer<Integer> listAction = n -> {
-            System.out.println("method4: For each lambda implementation value :: " + n);
-        };
-        list.forEach(listAction);
-
-        // Method 5: Implicit lambda function
+        Predicate<Integer> isEven = n-> n%2 ==0;
+        
         list.forEach(n -> {
-            System.out.println("method5: For each implementation value :: " + n);
+            if(isEven.test(n)) {
+            	System.out.println("Even numbers: "+n);
+            }
         });
     }
 }
